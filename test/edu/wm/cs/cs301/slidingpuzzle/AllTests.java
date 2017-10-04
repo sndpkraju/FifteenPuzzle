a8399e82214e41d7ef8aef8914ad7e0db1d205f2
sndpkraju <31968939+sndpkraju@users.noreply.github.com>
2017-10-04 13:38:52 -0400

    first commit

---
+++ test/edu/wm/cs/cs301/slidingpuzzle/AllTests.java
@@ -0,0 +1,11 @@
+package edu.wm.cs.cs301.slidingpuzzle;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SimplePuzzleStateBasicsTest.class, SimplePuzzleStateTest.class })
+public class AllTests {
+
+}
